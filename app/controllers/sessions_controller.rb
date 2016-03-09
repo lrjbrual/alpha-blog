55a09321f9f11b1cc3c58587e689ea6c884a9800
lrjbrual <lrjbrual@live.com>
2016-03-09 14:07:42 +0000

    Adding Login and logout Username

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,8 +7,8 @@ class SessionsController < ApplicationController
       user = User.find_by(email: params[:session][:email].downcase)
       if user && user.authenticate(params[:session][:password])
         session[:user_id] = user.id
-        flash[:succes] = "You have successfully logged in"
-        redirect_to users_path(user)
+        flash[:success] = "You have successfully logged in"
+        redirect_to user_path(user)
       else
         flash.now[:danger] = "There was something wrong with your login information"
         render 'new'
@@ -16,6 +16,9 @@ class SessionsController < ApplicationController
     end
     
     def destroy
+      session[:user_id] = nil
+      flash[:success] = "You have logged out"
+      redirect_to root_path
     end
 
 end 
